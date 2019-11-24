bceba376e758fb7e0eaef95b6fcaf25e071c1179
Ilya <shangareev.work@gmail.com>
2019-11-24 19:57:57 +0500

    added moved dwarf and troll kill detection

--- scripts/backup.js
+++ scripts/backup.js
@@ -1,23 +1,12 @@
 area.addEventListener('click', (e) => {
-    const cell = e.target;
+    const cell = e.target;;
     if (cell.className === 'troll') {
         document.querySelectorAll('td').forEach((item) => {
             item.classList.remove('select-unit');
             item.classList.remove('select-move');
         })
         cell.classList.add('select-unit');
-        out = moveTroll(+cell.dataset.x, +cell.dataset.y);
-        out.move.forEach((item) => {
-            if (item.classList.contains('null')) {
-                item.classList.add('select-move');
-
-            }
-            if (item.classList.contains('dwarf')) {
-                item.classList.add('kill-him');
-            } else {
-
-            }
-        })
+        select = moveTroll(+cell.dataset.x, +cell.dataset.y);
     }
     if (cell.className === 'dwarf') {
         document.querySelectorAll('td').forEach((item) => {
@@ -25,62 +14,50 @@ area.addEventListener('click', (e) => {
             item.classList.remove('select-move');
         })
         cell.classList.add('select-unit');
-        out = moveDwarf(+cell.dataset.x, +cell.dataset.y);
-        out.move.forEach((item) => {
-            if (item.classList.contains('null')) {
-                item.classList.add('select-move');
-            }
-        })
+        select = moveDwarf(+cell.dataset.x, +cell.dataset.y);
     }
     if (cell.classList.contains('select-move')) {
-        if (out.unit.classList.contains('troll')) {
+        if (select.classList.contains('troll')) {
             cell.innerText = 'T';
             cell.className = 'troll';
         }
-        if (out.unit.classList.contains('dwarf')) {
+        if (select.classList.contains('dwarf')) {
             cell.innerText = 'D';
             cell.className = 'dwarf';
         }
-        out.unit.innerText = ''
-        out.unit.className = 'null';
+        select.innerText = ''
+        select.className = 'null';
         document.querySelectorAll('.select-move').forEach((item) => {
             item.innerText = '';
             item.classList.remove('select-move');
-        })
-        document.querySelectorAll('td').forEach((item) => {
-            item.classList.remove('select-unit');
-            item.classList.remove('select-move');
-            item.classList.remove('kill-him');
         })
     }
 })
 
 function moveTroll(x, y) {
     let selectUnit = document.querySelector('td[data-X="' + x + '"][data-Y="' + y + '"]');
-    let selectMove = [];
     for (let i = -1; i < 2; i++) {
         for (let k = -1; k < 2; k++) {
-            if (document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y + k) + '"]') != null) {
-                selectMove.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y + k) + '"]'));
+            if ((x + i) >= 0 && (y + k) >= 0 && (x + i) <= 14 && (y + k) <= 14) {
+                let select = document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y + k) + '"]');
+                if (select.className === 'null') {
+                    select.classList.add('select-move');
+                }
             }
         }
     }
-    let out = {
-        unit: selectUnit,
-        move: selectMove
-    }
-    return out;
+    return selectUnit;
 }
 
 function moveDwarf(x, y) {
     let selectUnit = document.querySelector('td[data-X="' + x + '"][data-Y="' + y + '"]');
-    let selectMove = [];
+    let select = [];
     for (let i = 1; i < 15; i++) {
         if (document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y) + '"]') != null) {
             if (document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -88,7 +65,7 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -96,7 +73,7 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x) + '"][data-Y="' + (y + i) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x) + '"][data-Y="' + (y + i) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x) + '"][data-Y="' + (y + i) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -104,7 +81,7 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x) + '"][data-Y="' + (y - i) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x) + '"][data-Y="' + (y - i) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x) + '"][data-Y="' + (y - i) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -112,7 +89,7 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y + i) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y + i) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y + i) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -120,7 +97,7 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y - i) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y - i) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y - i) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -128,7 +105,7 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y + i) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y + i) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x - i) + '"][data-Y="' + (y + i) + '"]'));
         }
     }
     for (let i = 1; i < 15; i++) {
@@ -136,12 +113,13 @@ function moveDwarf(x, y) {
             if (document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y - i) + '"]').classList.contains('null') != true) {
                 break
             }
-            selectMove.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y - i) + '"]'));
+            select.push(document.querySelector('td[data-X="' + (x + i) + '"][data-Y="' + (y - i) + '"]'));
         }
     }
-    let out = {
-        unit: selectUnit,
-        move: selectMove
-    }
-    return out;
+    select.forEach((item) => {
+        if (item.className === 'null') {
+            item.classList.add('select-move');
+        }
+    })
+    return selectUnit;
 }
